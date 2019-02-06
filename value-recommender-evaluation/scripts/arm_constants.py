445883fb60747c031a91ffa903a73ed90d3c99ae
Marcos Martinez <marcos.martinez.romero@gmail.com>
2019-02-06 12:25:52 -0800

    Add utilities and fragment of original NCBI BioSamples XML file

--- value-recommender-evaluation/scripts/arm_constants.py
+++ value-recommender-evaluation/scripts/arm_constants.py
@@ -7,8 +7,9 @@ class BIOSAMPLES_DB(Enum):
     NCBI = 1
     EBI = 2
 
+DATA_BASE_PATH = "data"
+OUTPUT_BASE_PATH = "tmp"
 
-BASE_PATH = "tmp"
 
 ####################
 # DOWNLOAD SAMPLES #
@@ -19,21 +20,21 @@ EBI_DOWNLOAD_URL = 'https://www.ebi.ac.uk/biosamples/api/samples'
 EBI_DOWNLOAD_MAX_SIZE_PER_PAGE = 1000
 EBI_DOWNLOAD_PAGES_PER_FILE = 1
 EBI_DOWNLOAD_MAX_PAGES = -1
-EBI_DOWNLOAD_OUTPUT_FOLDER = BASE_PATH + "/samples/ebi_samples/original"
+EBI_DOWNLOAD_OUTPUT_FOLDER = OUTPUT_BASE_PATH + "/samples/ebi_samples/original"
 
 ##################
 # FILTER SAMPLES #
 ##################
 
 # Input parameters for 'ncbi_biosamples_2_filter.py'
-NCBI_FILTER_INPUT_FILE = BASE_PATH + '/samples/ncbi_samples/original/2018-03-09-biosample_set.xml'
-NCBI_FILTER_OUTPUT_FILE = BASE_PATH + '/samples/ncbi_samples/filtered/biosample_result_filtered.xml'
+NCBI_FILTER_INPUT_FILE = DATA_BASE_PATH + '/samples/ncbi_samples/original/2018-03-09-biosample_set.xml'
+NCBI_FILTER_OUTPUT_FILE = OUTPUT_BASE_PATH + '/samples/ncbi_samples/filtered/biosample_result_filtered.xml'
 NCBI_FILTER_RELEVANT_ATTS = ['sex', 'tissue', 'disease', 'cell_type', 'cell type', 'cell_line', 'cell line', 'ethnicity']
 NCBI_FILTER_MIN_RELEVANT_ATTS = 3
 
 # Input parameters for 'ebi_biosamples_2_filter.py'
-EBI_FILTER_INPUT_FOLDER = BASE_PATH + '/samples/ebi_samples/original'
-EBI_FILTER_OUTPUT_FOLDER = BASE_PATH + '/samples/ebi_samples/filtered'
+EBI_FILTER_INPUT_FOLDER = DATA_BASE_PATH + '/samples/ebi_samples/original'
+EBI_FILTER_OUTPUT_FOLDER = OUTPUT_BASE_PATH + '/samples/ebi_samples/filtered'
 EBI_FILTER_MAX_SAMPLES_PER_FILE = 10000
 EBI_FILTER_RELEVANT_ATTS = ['sex', 'organismPart', 'cellLine', 'cellType', 'diseaseState', 'ethnicity']
 EBI_FILTER_MIN_RELEVANT_ATTS = 3
@@ -47,38 +48,38 @@ EBI_INSTANCES_TRAINING_SET_SIZE = 102000
 EBI_INSTANCES_TESTING_SET_SIZE = 18000
 EBI_INSTANCES_MAX_FILES_PER_FOLDER = 10000
 EBI_INSTANCES_INPUT_PATH = EBI_FILTER_OUTPUT_FOLDER
-EBI_INSTANCES_OUTPUT_BASE_PATH = BASE_PATH + '/cedar_instances/ebi_cedar_instances'
-EBI_INSTANCES_TRAINING_BASE_PATH = EBI_INSTANCES_OUTPUT_BASE_PATH + '/training'
-EBI_INSTANCES_TESTING_BASE_PATH = EBI_INSTANCES_OUTPUT_BASE_PATH + '/testing'
+EBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH = OUTPUT_BASE_PATH + '/cedar_instances/ebi_cedar_instances'
+EBI_INSTANCES_TRAINING_OUTPUT_BASE_PATH = EBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH + '/training'
+EBI_INSTANCES_TESTING_OUTPUT_BASE_PATH = EBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH + '/testing'
 EBI_INSTANCES_EXCLUDE_IDS = False
 EBI_INSTANCES_EXCLUDED_IDS_FILE_PATH = None
 EBI_INSTANCES_OUTPUT_BASE_FILE_NAME = 'ebi_biosample_instance'
-EBI_INSTANCES_EMPTY_BIOSAMPLE_INSTANCE_PATH = BASE_PATH + '/cedar_templates_and_reference_instances/ebi/ebi_biosample_instance_empty.json'
+EBI_INSTANCES_EMPTY_BIOSAMPLE_INSTANCE_PATH = OUTPUT_BASE_PATH + '/cedar_templates_and_reference_instances/ebi/ebi_biosample_instance_empty.json'
 
 # Input parameters for 'ncbi_biosample_2_to_cedar_instances.py'
 # NCBI_INSTANCES_TRAINING_SET_SIZE = 102000
 # NCBI_INSTANCES_TESTING_SET_SIZE = 0
 # NCBI_INSTANCES_MAX_FILES_PER_FOLDER = 10000
 # NCBI_INSTANCES_INPUT_PATH = NCBI_FILTER_OUTPUT_FILE
-# NCBI_INSTANCES_OUTPUT_BASE_PATH = BASE_PATH + '/cedar_instances/ncbi_cedar_instances'
-# NCBI_INSTANCES_TRAINING_BASE_PATH = NCBI_INSTANCES_OUTPUT_BASE_PATH + '/training'
-# NCBI_INSTANCES_TESTING_BASE_PATH = NCBI_INSTANCES_OUTPUT_BASE_PATH + '/testing'
+# NCBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH = OUTPUT_BASE_PATH + '/cedar_instances/ncbi_cedar_instances'
+# NCBI_INSTANCES_TRAINING_OUTPUT_BASE_PATH = NCBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH + '/training'
+# NCBI_INSTANCES_TESTING_OUTPUT_BASE_PATH = NCBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH + '/testing'
 # NCBI_INSTANCES_EXCLUDE_IDS = True
-# NCBI_INSTANCES_EXCLUDED_IDS_FILE_PATH = BASE_PATH + '/cedar_instances/ebi_cedar_instances/testing_ids.txt'
+# NCBI_INSTANCES_EXCLUDED_IDS_FILE_PATH = OUTPUT_BASE_PATH + '/cedar_instances/ebi_cedar_instances/testing_ids.txt'
 # NCBI_INSTANCES_OUTPUT_BASE_FILE_NAME = 'ncbi_biosample_instance'
-# NCBI_INSTANCES_EMPTY_BIOSAMPLE_INSTANCE_PATH = BASE_PATH + '/cedar_templates_and_reference_instances/ncbi/ncbi_biosample_instance_empty.json'
+# NCBI_INSTANCES_EMPTY_BIOSAMPLE_INSTANCE_PATH = OUTPUT_BASE_PATH + '/cedar_templates_and_reference_instances/ncbi/ncbi_biosample_instance_empty.json'
 
 NCBI_INSTANCES_TRAINING_SET_SIZE = 0
 NCBI_INSTANCES_TESTING_SET_SIZE = 18000
 NCBI_INSTANCES_MAX_FILES_PER_FOLDER = 10000
 NCBI_INSTANCES_INPUT_PATH = NCBI_FILTER_OUTPUT_FILE
-NCBI_INSTANCES_OUTPUT_BASE_PATH = BASE_PATH + '/cedar_instances/ncbi_cedar_instances'
-NCBI_INSTANCES_TRAINING_BASE_PATH = NCBI_INSTANCES_OUTPUT_BASE_PATH + '/training'
-NCBI_INSTANCES_TESTING_BASE_PATH = NCBI_INSTANCES_OUTPUT_BASE_PATH + '/testing'
+NCBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH = OUTPUT_BASE_PATH + '/cedar_instances/ncbi_cedar_instances'
+NCBI_INSTANCES_TRAINING_OUTPUT_BASE_PATH = NCBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH + '/training'
+NCBI_INSTANCES_TESTING_OUTPUT_BASE_PATH = NCBI_INSTANCES_OUTPUT_OUTPUT_BASE_PATH + '/testing'
 NCBI_INSTANCES_EXCLUDE_IDS = True
-NCBI_INSTANCES_EXCLUDED_IDS_FILE_PATH = BASE_PATH + '/cedar_instances/ncbi_ebi_training_ids.txt'
+NCBI_INSTANCES_EXCLUDED_IDS_FILE_PATH = OUTPUT_BASE_PATH + '/cedar_instances/ncbi_ebi_training_ids.txt'
 NCBI_INSTANCES_OUTPUT_BASE_FILE_NAME = 'ncbi_biosample_instance'
-NCBI_INSTANCES_EMPTY_BIOSAMPLE_INSTANCE_PATH = BASE_PATH + '/cedar_templates_and_reference_instances/ncbi/ncbi_biosample_instance_empty.json'
+NCBI_INSTANCES_EMPTY_BIOSAMPLE_INSTANCE_PATH = OUTPUT_BASE_PATH + '/cedar_templates_and_reference_instances/ncbi/ncbi_biosample_instance_empty.json'
 
 ##############################################
 # SYSTEM EVALUATION (arm_evaluation_main.py) #
@@ -88,30 +89,30 @@ EVALUATION_TRAINING_DB = BIOSAMPLES_DB.NCBI
 EVALUATION_TESTING_DB = BIOSAMPLES_DB.NCBI
 EVALUATION_USE_ANNOTATED_VALUES = True
 EVALUATION_EXTEND_URIS_WITH_MAPPINGS = True
-EVALUATION_MAPPINGS_FILE_PATH = BASE_PATH + '/cedar_instances_annotated/unique_values/mappings_merged.json'
+EVALUATION_MAPPINGS_FILE_PATH = OUTPUT_BASE_PATH + '/cedar_instances_annotated/unique_values/mappings_merged.json'
 EVALUATION_MAX_NUMBER_INSTANCES = 20000
 EVALUATION_CEDAR_API_KEY = ''
 
 # EVALUATION_TESTING_INSTANCES_BASE_FOLDERS = {
-#     "NCBI": BASE_PATH + '/cedar_instances/ncbi_cedar_instances/testing',
-#     "EBI": BASE_PATH + '/cedar_instances/ebi_cedar_instances/testing'
+#     "NCBI": OUTPUT_BASE_PATH + '/cedar_instances/ncbi_cedar_instances/testing',
+#     "EBI": OUTPUT_BASE_PATH + '/cedar_instances/ebi_cedar_instances/testing'
 # }
 
 # EVALUATION_TESTING_INSTANCES_ANNOTATED_BASE_FOLDERS = {
-#     "NCBI": BASE_PATH + '/cedar_instances_annotated/ncbi_cedar_instances/testing',
-#     "EBI": BASE_PATH + '/cedar_instances_annotated/ebi_cedar_instances/testing'
+#     "NCBI": OUTPUT_BASE_PATH + '/cedar_instances_annotated/ncbi_cedar_instances/testing',
+#     "EBI": OUTPUT_BASE_PATH + '/cedar_instances_annotated/ebi_cedar_instances/testing'
 # }
 
 EVALUATION_TESTING_INSTANCES_ANNOTATED_BASE_FOLDERS = {
-    "NCBI": BASE_PATH + '/cedar_instances_annotated/ncbi_cedar_instances/testing',
-    "EBI": BASE_PATH + '/cedar_instances_annotated/ebi_cedar_instances_different_ontologies/testing'
+    "NCBI": OUTPUT_BASE_PATH + '/cedar_instances_annotated/ncbi_cedar_instances/testing',
+    "EBI": OUTPUT_BASE_PATH + '/cedar_instances_annotated/ebi_cedar_instances_different_ontologies/testing'
 }
 
-EVALUATION_NCBI_MOST_FREQUENT_VALUES_PATH = BASE_PATH + '/most_frequent_values/ncbi_frequent_values.json'
-EVALUATION_EBI_MOST_FREQUENT_VALUES_PATH = BASE_PATH + '/most_frequent_values/ebi_frequent_values.json'
-EVALUATION_NCBI_MOST_FREQUENT_VALUES_ANNOTATED_PATH = BASE_PATH + '/most_frequent_values/ncbi_annotated_frequent_values.json'
-#EVALUATION_EBI_MOST_FREQUENT_VALUES_ANNOTATED_PATH = BASE_PATH + '/most_frequent_values/ebi_annotated_frequent_values.json'
-EVALUATION_EBI_MOST_FREQUENT_VALUES_ANNOTATED_PATH = BASE_PATH + '/most_frequent_values/ebi_annotated_diff_onts_frequent_values.json'
+EVALUATION_NCBI_MOST_FREQUENT_VALUES_PATH = OUTPUT_BASE_PATH + '/most_frequent_values/ncbi_frequent_values.json'
+EVALUATION_EBI_MOST_FREQUENT_VALUES_PATH = OUTPUT_BASE_PATH + '/most_frequent_values/ebi_frequent_values.json'
+EVALUATION_NCBI_MOST_FREQUENT_VALUES_ANNOTATED_PATH = OUTPUT_BASE_PATH + '/most_frequent_values/ncbi_annotated_frequent_values.json'
+#EVALUATION_EBI_MOST_FREQUENT_VALUES_ANNOTATED_PATH = OUTPUT_BASE_PATH + '/most_frequent_values/ebi_annotated_frequent_values.json'
+EVALUATION_EBI_MOST_FREQUENT_VALUES_ANNOTATED_PATH = OUTPUT_BASE_PATH + '/most_frequent_values/ebi_annotated_diff_onts_frequent_values.json'
 
 EVALUATION_READ_TEST_INSTANCES_FROM_CEDAR = False  # If false, the instances are read from a local folder
 EVALUATION_VR_SERVER = 'https://valuerecommender.metadatacenter.orgx/'
@@ -120,8 +121,8 @@ EVALUATION_NCBI_TEMPLATE_ID = 'https://repo.metadatacenter.orgx/templates/eef6f3
 # EVALUATION_EBI_TEMPLATE_ID = 'https://repo.metadatacenter.orgx/templates/6b6c76e6-1d9b-4096-9702-133e25ecd140'
 EVALUATION_EBI_TEMPLATE_ID = 'https://repo.metadatacenter.orgx/templates/80766e2b-0629-49f2-ba77-224a88739ad7' # diff onts
 
-EVALUATION_UNIQUE_VALUES_ANNOTATED_FILE_PATH = BASE_PATH + '/cedar_instances_annotated/unique_values/unique_values_annotated.json'
-EVALUATION_OUTPUT_RESULTS_PATH = BASE_PATH + '/results'
+EVALUATION_UNIQUE_VALUES_ANNOTATED_FILE_PATH = OUTPUT_BASE_PATH + '/cedar_instances_annotated/unique_values/unique_values_annotated.json'
+EVALUATION_OUTPUT_RESULTS_PATH = OUTPUT_BASE_PATH + '/results'
 
 EVALUATION_NCBI_FIELD_DETAILS = {'sex': {'path': 'sex', 'json_path': '$.sex'},
                       'tissue': {'path': 'tissue', 'json_path': '$.tissue'},
